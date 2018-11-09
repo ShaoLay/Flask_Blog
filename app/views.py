1640b69214d33ec9b756fd88d0d1261352d85a61
hamdi <javs_shao@163.com>
2018-11-09 11:55:30 +0800

    添加网站首页欢迎（前后端）

--- app/views.py
+++ app/views.py
@@ -6,6 +6,17 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'nickname':"Miguel"}
+    posts = [
+        {
+            'author':{'nickname':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author':{'nickname':'Susan'},
+            'body':'The Avengers movie was so cool!'
+        }
+    ]
     return render_template("index.html",
                            title = 'Home',
-                           user = user)
+                           user = user,
+                           posts = posts)
