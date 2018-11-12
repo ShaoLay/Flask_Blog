858a7d630e519c6de7517e523e966a69661ec635
hamdi <javs_shao@163.com>
2018-11-12 12:22:42 +0800

    SQLAlchemy导入数据库扩展

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from config import Config
 
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
 # 创建SQLALCHEMY对象
 db = SQLAlchemy(app)
 
--- config.py
+++ config.py
@@ -1,15 +1,22 @@
-from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+# from app import app
 
 
-# class Config(object):
-#激活跨站点请求伪造保护
-CSRF_ENABLED = True
-# 项目秘钥
-app.SECRET_KEY = 'q7pBNcWPgmF6BqB6b5VICF7z7pI'
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
 
-# 配置Mysql数据库连接信息
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:970202@127.0.0.1:3306/Flask_Blog'
-# 是否追踪数据库的修改：不追踪(节省开销)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# 查询时会显示原始SQL语句
-app.config['SQLALCHEMY_ECHO'] = True
+    # 数据库的配置信息
+    # 配置Mysql数据库连接信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:970202@127.0.0.1:3306/Flask_Blog'
+    # 是否追踪数据库的修改：不追踪(节省开销)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# app.config.from_object(Config)
+# db = SQLAlchemy(app)
+
+
+
+
