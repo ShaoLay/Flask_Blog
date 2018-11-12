3867ef9b37d8e82fbfbe206d653a1e5fd2fd3b19
hamdi <javs_shao@163.com>
2018-11-12 12:40:24 +0800

    Flask_Script与数据库迁移扩展

--- manage.py
+++ manage.py
@@ -1,5 +1,13 @@
-from app import app
+from flask_script import Manager
 
+from app import app, db
+from flask_migrate import Migrate, MigrateCommand
+
+
+manager = Manager(app)
+Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
