bd519bd7fb931cf276d47920d8d461956bc7c109
hamdi <javs_shao@163.com>
2018-11-13 14:56:42 +0800

    注册之前后端实现

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired()])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登录')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    email = StringField('电子邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    password2 = PasswordField(
+        '确认密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('注册')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('用户名已存在.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('这个邮箱已经注册过用户了.')
--- app/index/views.py
+++ app/index/views.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_user
 from werkzeug.urls import url_parse
 
 from app import app, db, openid, login
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -40,6 +40,20 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='登录', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 
