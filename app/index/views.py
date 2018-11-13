ad109d9dd96e4c1e75aa27382f98d411ff423d15
hamdi <javs_shao@163.com>
2018-11-13 14:15:40 +0800

    登录_查询用户名或密码是否正确

--- app/index/views.py
+++ app/index/views.py
@@ -1,6 +1,10 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
+from flask import render_template, flash, redirect, url_for,session,request,g
+from flask_login import current_user, login_user
+from werkzeug.urls import url_parse
+
+from app import app, db, openid, login
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -20,15 +24,23 @@ def index():
     return render_template('index.html', title='首页', user=user, posts=posts)
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('正在登录的用户是：   {}'.format(
-            form.username.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='登录', form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('登录失败：用户名或密码不正确！')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='登录', form=form)
+
 
 
 @app.route('/favicon.ico',methods=['GET'])
