d6a8f563e07dae3a53316901b1c75bcabbd44af8
hamdi <javs_shao@163.com>
2018-11-13 20:54:57 +0800

    定制HTTP错误处理器（页面出现错误，给用户呈现的页面）

--- app/index/views.py
+++ app/index/views.py
@@ -99,3 +99,12 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html',title='修改个人资料',form=form)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
--- config.py
+++ config.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """工程配置信息"""
-    DEBUG = True
-
+    DEBUG = False
+    # DEBUG = True
     # 数据库的配置信息
     # 配置Mysql数据库连接信息
     SQLALCHEMY_DATABASE_URI = 'mysql://root:970202@127.0.0.1:3306/Flask_Blog'
